d18c45917029961ce6a0d1bc04bedd2462f221bc
Wodorek <Krol.w94@gmail.com>
2023-05-23 21:09:18 +0200

    Update route.ts

--- app/api/getUsedChars/route.ts
+++ app/api/getUsedChars/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: Request) {
   const translated = await fetch(`https://api-free.deepl.com/v2/usage`, {
     method: 'GET',
     headers: {
