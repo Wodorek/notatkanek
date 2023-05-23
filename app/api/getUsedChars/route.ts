3c4e24970abc84f3cc9cef95d2923618cf4a9cf9
Wodorek <Krol.w94@gmail.com>
2023-05-23 20:16:56 +0200

    Create route.ts

---
+++ app/api/getUsedChars/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const translated = await fetch(`https://api-free.deepl.com/v2/usage`, {
+    method: 'GET',
+    headers: {
+      Authorization: `DeepL-Auth-Key ${process.env.DEEPL_AUTH!}`,
+    },
+  }).then((resp) => {
+    return resp.json();
+  });
+
+  return NextResponse.json({ translated });
+}
